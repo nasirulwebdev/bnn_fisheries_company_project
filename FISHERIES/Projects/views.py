71e089d8100dd0923f474b1c4e5b23f8dd5ac597
nasirul <mdnasirulislamnahidweb@gmail.com>
2026-01-28 12:10:35 +0600

    Fixed template and URLs

--- FISHERIES/Projects/views.py
+++ FISHERIES/Projects/views.py
@@ -5,4 +5,4 @@ def project_list_view(request):
     projects = Project.objects.filter(is_active=True)
     context = {'projects': projects}
     # image + video support template
-    return render(request, 'Projects/projects_images+video.html', context)
+    return render(request, 'Projects/projects_images_video.html', context)
